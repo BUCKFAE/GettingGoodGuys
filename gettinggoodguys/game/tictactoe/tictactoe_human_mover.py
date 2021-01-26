e60eeeba087d0b524f31a3b15d3fe1b9655ab3d1
steven846 <stevenglauer@t-online.de>
2021-01-26 10:35:19 +0100

    Merge

--- gettinggoodguys/game/tictactoe/tictactoe_human_mover.py
+++ gettinggoodguys/game/tictactoe/tictactoe_human_mover.py
@@ -1,16 +1,17 @@
 from typing import Tuple
 
 from gettinggoodguys.game.mover import Mover
+from gettinggoodguys.game.player_type import PlayerType
 
 
 class TicTacToeHumanMover(Mover):
 
     def get_next_move(self, game, user_inputs) -> Tuple[int, int]:
+        print(user_inputs)
 
         # pressed_location = user_inputs[0] / 300
         return 0, 0
         pass
 
     def get_player_type(self):
-        # TODO: Return player type again
-        pass
+        return PlayerType.HUMAN
--- gettinggoodguys/game/tictactoe/tictactoe_random_mover.py
+++ gettinggoodguys/game/tictactoe/tictactoe_random_mover.py
@@ -1,9 +1,13 @@
 import random
 
 from gettinggoodguys.game.mover import Mover
+from gettinggoodguys.game.player_type import PlayerType
 
 
 class TicTacToeRandomMover(Mover):
+
+    def get_player_type(self):
+        return PlayerType.AI
 
     def get_next_move(self, game, user_inputs):
         possible_moves = game.get_possible_moves()
